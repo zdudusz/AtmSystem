774a2161e95bd53a239c6dbc2de6aa6fc35e6efc
zdudusz <eduardogamese8@gmail.com>
2026-02-15 18:15:03 -0300

    Users e Passwords padrões definidas.

---
+++ AtmJavaProject/src/Atm/Config.java
@@ -0,0 +1,39 @@
+package Atm;
+
+public class Config {
+    private double saldo;
+    private String nome;
+    private String senha;
+    public boolean verificador = false;
+
+    public Config(String _nome, String _senha,double saldoInicial){
+        this.nome = _nome;
+        this.senha = _senha;
+        this.saldo = saldoInicial;
+    }
+    String[] users = {
+            "Owner",
+            "BirdPerson",
+            "Rick",
+            "iLove",
+            "Garry",
+            "Jerry Smith",
+            "Morty",
+            "Admin",
+            this.nome
+    };
+    String[] passwords = {
+            "1234",
+            "Thamy",
+            "Morty",
+            "Coffe",
+            "SpongeBob",
+            "Beth",
+            "Rick",
+            "Admin",
+            this.senha
+    };
+
+
+
+}
