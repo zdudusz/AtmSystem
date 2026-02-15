11b3b994c0dbaaef33de49a341026b0429fe3a49
zdudusz <eduardogamese8@gmail.com>
2026-02-15 20:42:31 -0300

    Adicionei o metodo limparConsole()

--- AtmJavaProject/src/Main/Main.java
+++ AtmJavaProject/src/Main/Main.java
@@ -2,10 +2,22 @@ package Main;
 import Atm.Config;
 import java.util.Scanner;
 public class Main {
+    public static void limparConsole() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (Exception e) {
+        }
+    }
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     boolean acessoAoMenu = false;
     Config user = null;
+    limparConsole();
     System.out.println("Bem vindo ao sistema ATM 24h! \n Você tem alguma conta? \n 1-SIM \n 2-NAO");
     String temConta = sc.next();
     boolean verifier = false;
@@ -19,10 +31,12 @@ public static void main(String[] args) {
              user = new Config(login,senha);
          acessoAoMenu = true;}
          else{
+             limparConsole();
              System.out.print("CONTA NAO EXISTE! ");
          }
     }
     else if(temConta.equals("2")){
+        limparConsole();
         System.out.println("Deseja criar uma conta? \n 1-SIM \n 2-NAO");
         sc.nextLine();
         String flag = sc.next();
@@ -37,25 +51,30 @@ public static void main(String[] args) {
              user = new Config(_login,_senha,saldoInicial);
             acessoAoMenu = true;
         } else if (flag.equals("2")) {
+            limparConsole();
             System.out.print("SISTEMA FINALIZADO...");
         }
     }
     if(acessoAoMenu == true){
         while(true) {
+            limparConsole();
             System.out.println("|MENU DA CONTA| \n 1-USER INFO \n 2-SACAR \n 3-DEPOSITAR \n QUALQUER OUTRA OPÇAO-SAIR");
             String flagMenu = sc.next();
             if (flagMenu.equals("1")) {
                 System.out.println(user.toString()+ "\n  DIGITE ALGO PARA CONTINUAR");
                 String a = sc.next();
-
-
             } else if (flagMenu.equals("2")) {
                 System.out.println("Quanto você quer sacar: ");
                 double valorSacado = sc.nextDouble();
                 if (valorSacado > user.getSaldo()) {
+                    limparConsole();
                     System.out.print("Saldo insuficiente");
+                    System.out.print("\nDIGITE ALGO PARA CONTINUAR");
+                    String continuar = sc.next();
+                    limparConsole();
                 }
-                user.sacarSaldo(valorSacado);
+                else{
+                user.sacarSaldo(valorSacado);}
             } else if (flagMenu.equals("3")) {
                 System.out.println("VALOR DEPOSITADO: ");
                 double valorDepositado = sc.nextDouble();
